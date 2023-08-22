984081ed5813b75102b74ac437b0542a108d6025
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-22 17:25:06 +0200

    loving vue thus far

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>Dev Josh Timer Game</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+
+<!-- the BLOCK COMPONENT -->
+
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+
+// IMPORTING THE BLOCK COMPONENT
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+
+export default {
+  name: 'App',
+  // REGISTERING THE BLOCK COMPONENT
+  components: {Block, Results},
+  data(){
+    return {
+    isPlaying: false,
+    delay: null,
+    score: null,
+    showResults: false,
+    
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000,
+      this.isPlaying = true
+      this.showResults = false
+    },
+
+    endGame(reactionTime){
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResults = true
+    }
+
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  margin: 10px;
+  cursor: pointer;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+    <p> Reaction Time - {{score }} ms</p>
+    <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+
+    mounted(){
+        if (this.score < 250) {
+            this.rank = 'Ninja - Finger '
+        }
+        else if (this.score < 400) {
+            this.rank = 'Rapid - Reflexes'
+        }
+        else {
+            this.rank = 'Sail pace ....'
+        }
+    }
+}
+</script>
+
+<style>
+        .rank {
+            font-size: 1.4rem;
+            color: #0faf87;
+            font-weight: bold;
+        }
+</style>
