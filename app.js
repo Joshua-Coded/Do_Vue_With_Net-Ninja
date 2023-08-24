c550fda7a1b63b0c8a3174f7df3787aa1a7b2060
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-24 15:22:17 +0200

    computed properties in vue

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const app = Vue.createApp({
             toggleFav(book){
                 book.isFav = !book.isFav
             }
+    },
+
+    computed: {
+        filteredBooks(){
+            return this.books.filter((book) => book.isFav )
+        }
     }
 });
 
