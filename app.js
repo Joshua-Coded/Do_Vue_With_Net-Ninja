23e65f1c7bb6b867590528f24f2f3450a22652b5
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-24 14:40:50 +0200

    v-binding images

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = Vue.createApp({
             url: 'https://afri-homes-3pyp.vercel.app/',
             showBook: true,
             books: [
-                {title: "name of the wind", author: "patrick"},
-                {title: "the way of kings", author: "brandon sanderson"},
-                {title: "the final empire", author: "brandon sanderson"},
-                {title: "name of the wind", author: "joshua alana"},
+                {title: 'name of the wind', author: 'patrick', img: 'assets/1.jpg'},
+                {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.png'},
+                {title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.JPG'},
+                {title: 'name of the wind', author: 'joshua alana', img: 'assets/4.png'},
             ]
         }
     },
