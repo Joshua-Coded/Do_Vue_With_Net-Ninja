ca333a7f618415acd5abb33005c07bfc83839100
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-24 15:01:36 +0200

    dynamic classes in vue

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const app = Vue.createApp({
             toggleShowBooK(){
                 this.showBook = !this.showBook
             },
+            toggleFav(book){
+                book.isFav = !book.isFav
+            }
     }
 });
 
