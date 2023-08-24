2ac00cc98d3172797f9f089f22fc980927571c71
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-24 09:14:22 +0200

    mouse events

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const app = Vue.createApp({
             title: "the finale",
             author: "Brandon sanderson",
             age: 80,
+            x: 0,
+            y: 0,
         }
     },
 
     methods: {
             toggleShowBooK(){
                 this.showBook = !this.showBook
+            },
+
+            handleEvent(e, data){
+                    console.log(e)
+                    if(data){
+                        console.log(data)
+                    }
+            },
+            handleMouseMove(e){
+                this.x = e.offsetX
+                this.y = e.offsetY
             }
     }
 });
